0dc8242f4170897955027bb6b380ce5199f34392
xiaodi007 <334830452@qq.com>
2018-10-08 12:06:25 +0800

    plot

--- 1.R
+++ 1.R
@@ -1,14 +1,57 @@
 library(dplyr)
 library(readr)
 library(ggplot2)
-patient_index_data <- read_csv("F:/Hospital/patient_index_data.csv")
+patient_index_data <- read_csv("./hospital.csv")
+
 
 ###NBDBJGBZ CysC MTP24H
 patient_data <- patient_index_data %>% 
-  select(id_card, MTP24H, NBDBJGBZ) %>% 
-  filter(is.na(MTP24H) != TRUE, is.na(NBDBJGBZ) != TRUE)
+  mutate(ageEffect = if_else(gender == '男', 1, 0.742)) %>% 
+  mutate(GFR =  30849 * (CRE^(-1.154)) * (age^(-0.203)) * ageEffect) %>% 
+  mutate(GFR_CLASS = case_when(
+    GFR >= 90 ~ "正常",
+    GFR >= 60 & GFR <90 ~ "轻度下降",
+    GFR >= 30 & GFR <60 ~ "中度下降",
+    GFR >= 15 & GFR <30 ~ "重度度下降",
+    GFR <15             ~ "肾衰竭",
+    TRUE                ~ "未知"
+    
+  ))
 
-ggplot(patient_data, aes(MTP24H, NBDBJGBZ)) + 
-  geom_point() #+ 
-ylim(0, 22) + xlim(0, 15)#+
+
+
+#CysC GFR
+CysC_GFR_plot <- patient_data %>% 
+  select(CysC, GFR) %>% 
+  filter(is.na(CysC) != TRUE, is.na(GFR) != TRUE)
+
+ggplot(CysC_GFR_plot, aes(CysC, GFR)) + 
+  geom_point() + 
+xlim(0, 50) + ylim(0, 150)+
 geom_smooth()
+
+
+
+#NBDBJGBZ GFR
+NBDBJGBZ_GFR_plot <- patient_data %>% 
+  select(NBDBJGBZ, GFR) %>% 
+  filter(is.na(NBDBJGBZ) != TRUE, is.na(GFR) != TRUE)
+
+ggplot(NBDBJGBZ_GFR_plot, aes(NBDBJGBZ, GFR)) + 
+  geom_point() + 
+  #xlim(0, 50) + 
+    ylim(0, 150)+
+  geom_smooth()
+
+
+
+#MTP24H GFR
+MTP24H_GFR_plot <- patient_data %>% 
+  select(MTP24H, GFR) %>% 
+  filter(is.na(MTP24H) != TRUE, is.na(GFR) != TRUE)
+
+ggplot(MTP24H_GFR_plot, aes(MTP24H, GFR)) + 
+  geom_point() + 
+  #xlim(0, 50) + 
+  ylim(0, 150)+
+  geom_smooth()

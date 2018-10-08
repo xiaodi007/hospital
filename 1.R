0990b657b80c6e6f6862c050ca2414cb25a5b546
xiaodi007 <334830452@qq.com>
2018-10-08 12:02:29 +0800

    caculate GFR plot Cysc NBDBJGBZ MTP24H with GFR

--- 1.R
+++ 1.R
@@ -2,11 +2,13 @@ library(dplyr)
 library(readr)
 library(ggplot2)
 patient_index_data <- read_csv("F:/Hospital/patient_index_data.csv")
-patient_data <- patient_index_data %>% 
-  select(CysC, MTP24H) %>% 
-  filter(is.na(CysC) != TRUE, is.na(MTP24H) != TRUE)
 
-ggplot(patient_data, aes(CysC, MTP24H)) + 
-  geom_point() + 
-  ylim(0, 22) + xlim(0, 15)#+
-  geom_smooth()
+###NBDBJGBZ CysC MTP24H
+patient_data <- patient_index_data %>% 
+  select(id_card, MTP24H, NBDBJGBZ) %>% 
+  filter(is.na(MTP24H) != TRUE, is.na(NBDBJGBZ) != TRUE)
+
+ggplot(patient_data, aes(MTP24H, NBDBJGBZ)) + 
+  geom_point() #+ 
+ylim(0, 22) + xlim(0, 15)#+
+geom_smooth()
